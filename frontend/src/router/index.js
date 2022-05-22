383805ff5d636dbdfd013497826f0b0182703088
karino-t <kokuban.kumasan@gmail.com>
2022-05-22 15:55:15 +0900

    add simple wallet

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import DApp from '../views/DApp.vue'
+import Wallet from '../views/Wallet.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'DApp',
+    component: DApp
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/wallet',
+    name: 'Wallet',
+    component: Wallet
   }
 ]
 
---
+++ frontend/src/views/Wallet.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="wallet">
+    <div v-if="step === 'form'">
+      <div class="centerx labelx">
+        <vs-input label="URL" placeholder="wc:*****" v-model="wc_url"/>
+      </div>
+      <vs-button size="large" color="primary" type="filled" v-on:click="submit">Submit</vs-button>
+    </div>
+    <div v-if="step === 'approval'">
+      {{app_name}}
+      <vs-button size="large" color="primary" type="filled" v-on:click="approve">Approve</vs-button>
+      <vs-button size="large" color="primary" type="filled" v-on:click="reject">Reject</vs-button>
+    </div>
+    <div v-if="step === 'approved'">
+      Connected
+      {{app_name}}
+      <vs-button size="large" color="primary" type="filled" v-on:click="kill">Disconnect</vs-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import WalletConnect from "@walletconnect/client";
+
+export default {
+  name: "Wallet",
+  data() {
+    return {
+      wc_url: "",
+      accounts: "",
+      connector: "",
+      step: "form",
+      app_name: "",
+    }
+  },
+  components: {},
+  methods: {
+    approve: async function() {
+      this.connector.approveSession({
+        accounts: [
+          '0xee6983ACaCe98e766E5b78f0533EA74c6e734dE5',
+        ],
+        chainId: 3
+      })
+      this.step = "approved"
+    },
+    reject: async function() {
+      this.connector.rejectSession({
+        message: 'OPTIONAL_ERROR_MESSAGE'
+      })
+      this.step = "form"
+    },
+    kill: async function() {
+      this.connector.killSession()
+    },
+    showApproval: async function(peerMeta) {
+      this.step = "approval"
+      this.app_name = peerMeta
+    },
+    submit: async function () {
+      console.log(this.wc_url)
+      this.connector = new WalletConnect(
+        {
+          // Required
+          uri: this.wc_url,
+          // Required
+          clientMeta: {
+            description: "WalletConnect Developer App",
+            url: "https://walletconnect.org",
+            icons: ["https://walletconnect.org/walletconnect-logo.png"],
+            name: "WalletConnect",
+          },
+        }
+      );
+
+      this.connector.on("session_request", (error, payload) => {
+        if (error) {
+          throw error;
+        }
+        console.log(payload)
+        this.showApproval(payload.params[0].peerMeta)
+      });
+      this.connector.on("call_request", (error, payload) => {
+        if (error) {
+          throw error;
+        }
+        // TODO: eth_sendTransaction
+        console.log(payload.method)
+        console.log(payload.params)
+      });
+      this.connector.on("disconnect", (error, payload) => {
+        if (error) {
+          throw error;
+        }
+        console.log(payload)
+        this.step = "form"
+      });
+    },
+  }
+};
+</script>
